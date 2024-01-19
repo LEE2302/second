f802fbcd0ca7a7d48a10675868f6cb03b97685ca
LEE2302 <dltpdudqkqh1@gmail.com>
2024-01-20 02:56:57 +0900

    fix: 오류 수정과 주석추가

--- src/store/useStroe.ts
+++ src/store/useStroe.ts
@@ -27,18 +27,24 @@ const useQuantityPriceStroe = create<UseQuantityPriceStroeProps>((set) => ({
 
   //   액션 설정
   setItems: (items: ItemsPros[]) => set({ items }),
-  plusQuantity: (itmeId: string) => set((state) => {
+  // 플러스 버튼 클릭시 실행되는 액션 함수 => 매개변수로 Id를 받아온다.
+  plusQuantity: (itemId: string) => set((state) => {
+    // 주문 총 수량이 999개가 넘으면 바로 리턴
     if (state.totalQuantity < 999) {
       const updateProducts = state.items.map((itme) => {
-        if (itme.id === itmeId) {
+        // 받아온 Id와 데이터 매핑을 통해 동일한 id값을 가진 배열을 찾아 수량 + 1을 한다.
+        if (itme.id === itemId) {
           return { ...itme, quantity: (itme.quantity || 0) + 1 };
         }
+        // 수량이 새로 추가된 배열을 리턴
         return itme;
       });
+        // 총 가격 구하는 코드 => 필터를 통하여 수량이 있는 값만 찾은 후 => 수량과 해당 값을 곱함
       const totalPrice = updateProducts
         .filter((item) => item.quantity && item.quantity !== 0)
         .reduce((sum, item) => sum + (item.quantity || 0) * item.price, 0);
 
+      // 총 수량을 구하는 코드 => 수량이 새로 추가된 배열에 reduce사용하여 수량이 있는 값들만 더하기 없으면 0이 더해진다.
       const totalQuantity = updateProducts.reduce(
         (sum, item) => sum + (item.quantity || 0),
         0,
@@ -47,9 +53,10 @@ const useQuantityPriceStroe = create<UseQuantityPriceStroeProps>((set) => ({
     }
     return state;
   }),
-  minusQuantity: (itmeId: string) => set((state) => {
+  // 마이너스 클릭시 실행되는 액션 함수 => 매개변수로 Id를 받아온다.
+  minusQuantity: (itemId: string) => set((state) => {
     const updateProducts = state.items.map((itme) => {
-      if (itme.id === itmeId && itme.quantity && itme.quantity > 0) {
+      if (itme.id === itemId && itme.quantity && itme.quantity > 0) {
         return { ...itme, quantity: itme.quantity - 1 };
       }
       return itme;
@@ -58,7 +65,10 @@ const useQuantityPriceStroe = create<UseQuantityPriceStroeProps>((set) => ({
       .filter((item) => item.quantity && item.quantity !== 0)
       .reduce((sum, item) => sum + (item.quantity || 0) * item.price, 0);
 
-    const totalQuantity = state.totalQuantity > 0 ? state.totalQuantity - 1 : 0;
+    const totalQuantity = updateProducts.reduce(
+      (sum, item) => sum + (item.quantity || 0),
+      0,
+    );
     return { items: updateProducts, totalQuantity, totalPrice };
   }),
 }));
