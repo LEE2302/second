6633d750d8e84a04d9ea8446e5dae2602e6214d7
LEE2302 <dltpdudqkqh1@gmail.com>
2024-01-20 01:38:55 +0900

    feat: 3초후 order페이지 이동

--- src/pages/CompletePage.tsx
+++ src/pages/CompletePage.tsx
@@ -1,10 +1,32 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
 function CompletePage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 3초 후에 홈페이지로 이동
+    const timeoutId = setTimeout(() => {
+      navigate('/order');
+    }, 3000);
+
+    // 컴포넌트가 언마운트될 때 타이머를 클리어
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div>
+    <Container>
       <img src="/images/check-filled.png" alt="checkImg" />
       <p>주문이 완료되었습니다.</p>
-    </div>
+    </Container>
   );
 }
 
 export default CompletePage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,32 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
 function ErrorPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 3초 후에 홈페이지로 이동
+    const timeoutId = setTimeout(() => {
+      navigate('/order');
+    }, 3000);
+
+    // 컴포넌트가 언마운트될 때 타이머를 클리어
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div>
+    <Container>
       <p>주문에 실패하였습니다.</p>
       <p>다시 시도해주세요.</p>
-    </div>
+    </Container>
   );
 }
 
 export default ErrorPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
